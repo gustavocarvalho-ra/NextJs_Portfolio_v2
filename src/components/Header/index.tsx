d0d337ec312f4a6e7eabd5e8c229109362722570
GustavoCR021 <gustavoc1922@gmail.com>
2024-07-09 18:23:09 -0300

    fix error build

---
+++ src/components/Header/index.tsx
@@ -0,0 +1,15 @@
+"use client"
+
+import Link from "next/link";
+
+import { Head } from "./styles"; 
+
+export default function Header() {
+
+  return (
+    <Head>
+      
+      
+    </Head>
+  )
+}
---
+++ src/components/Header/styles.ts
@@ -0,0 +1,8 @@
+import styled from "styled-components";
+
+export const Head = styled.section `
+  width: 100vw;
+  height: 5vh;
+  background-color: red;
+  
+`
