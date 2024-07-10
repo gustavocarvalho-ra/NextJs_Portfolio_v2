586ad2a5d03e66ca949113a6f8186f5ab9a6d9ed
GustavoCR021 <gustavoc1922@gmail.com>
2024-07-09 21:53:31 -0300

    build: add styled components

---
+++ src/components/index.tsx
@@ -0,0 +1,16 @@
+"use client"
+
+import Link from "next/link"
+import Image from "next/image"
+
+import { Head } from "./styles"
+
+
+
+export default function Header() {
+  return(
+    <Head>
+      <p>test</p>
+    </Head>
+  )
+}
---
+++ src/components/styles.ts
@@ -0,0 +1,7 @@
+import styled from "styled-components";
+
+export const Head = styled.div `
+  width: 100vw;
+  height: 5vh;
+  background: red;
+`
