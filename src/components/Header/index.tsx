cdb3149f2079a04d0e713a0e8bd8e6ebaadcf178
GustavoCR021 <gustavoc1922@gmail.com>
2024-07-10 19:06:48 -0300

    style: ajust Header component

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -9,13 +9,15 @@ import { Head } from "./styles"
 
 export default function Header() {
   return(
-    <Head>
-      <h1>Gustavo <span>Carvalho</span></h1>
-      <div className="ctt">
-        <Link href={""}>whatsapp</Link>
-        <Link href={""}>email</Link>
-      </div>
+    <>
+      <Head>
+        <h1>Gustavo <span>Carvalho</span></h1>
+        <div className="ctt">
+          <Link href={""}>whatsapp</Link>
+          <Link href={""} style={{marginLeft: "1em"}}>email</Link>
+        </div>
+      </Head>
       <hr className="hr01"/>
-    </Head>
+    </>
   )
 }
--- src/components/Header/styles.ts
+++ src/components/Header/styles.ts
@@ -3,5 +3,12 @@ import styled from "styled-components";
 export const Head = styled.div `
   width: 100vw;
   height: 5vh;
-  background: red;
+  background: #021626;
+  display: flex;
+  align-items: center;
+  justify-content: space-around;
+
+  .ctt {
+
+  }
 `
