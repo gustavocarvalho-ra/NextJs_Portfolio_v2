6c2d4cc5f76514308cd6c212cae28f1209a76b2d
GustavoCR021 <gustavoc1922@gmail.com>
2024-07-10 19:10:54 -0300

    style: ajust page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,15 @@
+"use client"
+
 import Image from "next/image";
 import styles from "./page.module.css";
 import Header from "@/components/Header";
+import { Pag } from "./styles";
 
 export default function Home() {
   return (
-    <>
+    <Pag>
       <Header />
       
-    </>
+    </Pag>
   );
 }
---
+++ src/app/styles.ts
@@ -0,0 +1,7 @@
+import styled from "styled-components";
+
+export const Pag = styled.section `
+  width: 100vw;
+  height: 130vh;
+  background: #0a1218;
+`
