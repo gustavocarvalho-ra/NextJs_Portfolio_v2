5edf78866cf684a92ddc873d49ff67731419b40f
GustavoCR021 <gustavoc1922@gmail.com>
2024-07-11 15:56:38 -0300

    style: change h1 and h2 tags

--- src/components/Home/styles.ts
+++ src/components/Home/styles.ts
@@ -14,5 +14,17 @@ export const Hom = styled.div `
     justify-content: center;
     position: absolute;
     z-index: 1;
+
+    h1 {
+      font-size: 3.7em;
+      color: #d3d3d3;
+      font-weight: 600;
+    }
+
+    h2 {
+      font-size: 1.8em;
+      color: #d3d3d3;
+      font-weight: 200;
+    }
   }
 `
