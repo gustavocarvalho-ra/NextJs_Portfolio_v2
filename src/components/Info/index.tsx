28d05d6d8d9e78053c840f16d507268f7bd19da2
GustavoCR021 <gustavoc1922@gmail.com>
2024-07-11 00:38:10 -0300

    feat: add component info

---
+++ src/components/Info/index.tsx
@@ -0,0 +1,14 @@
+"use client"
+
+import Link from "next/link"
+
+import { Inf } from "./styles"
+
+
+export default function Info() {
+  return(
+    <>
+      <Inf />
+    </>
+  )
+}
---
+++ src/components/Info/styles.ts
@@ -0,0 +1,7 @@
+import styled from "styled-components";
+
+export const Inf = styled.div `
+  width: 100%;
+  height: 30vh;
+  background-color: red;
+`
