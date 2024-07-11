df332fae1d2f21e68e947dc5926470d1d737c4a7
GustavoCR021 <gustavoc1922@gmail.com>
2024-07-11 16:23:25 -0300

    build: add react-icons dependency

--- src/components/Home/index.tsx
+++ src/components/Home/index.tsx
@@ -4,6 +4,9 @@ import Link from "next/link"
 import Image from 'next/image';
 
 import { Hom } from "./styles"
+import { FaGithub } from "react-icons/fa";
+import { SiLinkedin } from "react-icons/si";
+import { TfiEmail } from "react-icons/tfi";
 
 
 export default function Home() {
@@ -11,11 +14,24 @@ export default function Home() {
     <Hom id="inicio">
       <Image src={"/back.jpeg"} alt="test" fill={true} style={{filter: "blur(12px)", objectFit: "cover", zIndex: "1"}} />
       <div className="apresentacao">
-        <h2>Olá!</h2>
-        <h1>Meu nome é Gustavo</h1>
-        <h2>Desenvolvedor Front-end</h2>
+        <div className="apre">
+          <h2>Olá!</h2>
+          <h1>Meu nome é Gustavo</h1>
+          <h2>Desenvolvedor Front-end</h2>
+        </div>
 
         <div className="icons">
+          <Link href={"https://github.com/gustavocarvalho-ra"} target="_blank">
+            <FaGithub style={{fontSize: '1.7em', color: '#666666' }}/>
+          </Link>
+
+          <Link href={""}>
+            <TfiEmail style={{fontSize: '1.7em', color: '#666666'}} />
+          </Link>
+          
+          <Link href={"https://www.linkedin.com/in/gustavo-carvalho-7bb361205/"} target="_blank">
+            <SiLinkedin style={{fontSize: '1.7em', color: '#666666'}}/>
+          </Link>
 
         </div>
       </div>
--- src/components/Home/styles.ts
+++ src/components/Home/styles.ts
@@ -14,17 +14,31 @@ export const Hom = styled.div `
     justify-content: center;
     position: absolute;
     z-index: 1;
+    gap: 2em;
+
+    .apre {
+      display: flex;
+      align-items: center;
+      flex-direction: column;
+      justify-content: center;
+      gap: .8em;
+    }
 
     h1 {
       font-size: 3.7em;
-      color: #d3d3d3;
-      font-weight: 600;
+      color: #b5b5b5;
+      font-weight: 800;
     }
 
     h2 {
       font-size: 1.8em;
-      color: #d3d3d3;
-      font-weight: 200;
+      color: #b5b5b5;
+      font-weight: 400;
+    }
+
+    .icons {
+      display: flex;
+      gap: 1.7em;
     }
   }
 `
