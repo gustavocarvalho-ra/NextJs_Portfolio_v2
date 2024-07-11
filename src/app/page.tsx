fdd9d06eed4bdfb5f1432e5bd7ef52120d28eb75
GustavoCR021 <gustavoc1922@gmail.com>
2024-07-11 00:42:22 -0300

    test component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,12 +4,13 @@ import Image from "next/image";
 import styles from "./page.module.css";
 import Header from "@/components/Header";
 import { Pag } from "./styles";
+import Info from "@/components/Info";
 
 export default function Home() {
   return (
     <Pag>
       <Header />
-      
+      <Info />
     </Pag>
   );
 }
