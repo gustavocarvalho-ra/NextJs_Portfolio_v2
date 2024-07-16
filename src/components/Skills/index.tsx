9ed8ed1018f6e12cc1a3cb3d997a9b59768b4d24
GustavoCR021 <gustavoc1922@gmail.com>
2024-07-15 21:47:08 -0300

    feat: add icons of skill

--- src/components/Skills/index.tsx
+++ src/components/Skills/index.tsx
@@ -1,9 +1,31 @@
 "use client"
 
 import { Ski } from "./styles"
+import {
+  DiHtml5,
+  DiCss3,
+  DiJsBadge,
+  DiReact,
+  DiSass,
+  DiJavascript1,
+  DiJavascript,
+  DiPython,
+  DiGithubBadge
+} from 'react-icons/di'
 
 
 export default function Skills() {
+
+  const skill = [
+    { id: "icon", name: "HTML5", icon: <DiHtml5 /> },
+    { id: "icon", name: "CSS3", icon: <DiCss3 /> },
+    { id: "icon", name: "JavaScript", icon: <DiJavascript1 /> },
+    { id: "icon", name: "React", icon: <DiReact /> },
+    { id: "icon", name: "Sass", icon: <DiSass /> },
+    { id: "icon", name: "Phyton", icon: <DiPython /> },
+    { id: "icon", name: "Git", icon: <DiGithubBadge /> },
+  ];
+
   return(
     <Ski id="skills">
       <div className="container">
